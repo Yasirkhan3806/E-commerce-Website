237440500d07eaa128dda7568d7bd3de570f5ce6
Yasirkhan3806 <154752345+Yasirkhan3806@users.noreply.github.com>
2024-03-15 22:13:20 +0500

    Add files via upload

---
+++ E-commerce.js
@@ -0,0 +1,69 @@
+// // Get references to the product elements and their corresponding images
+// const product1 = document.getElementById("idproduct1");
+
+// const product2 = document.getElementById("idproduct2");
+
+// const product3 = document.getElementById("idproduct3");
+
+
+// // Function to toggle the display of product elements and their images
+// function toggleProducts() {
+//   if ((product1.style.display !== "none") ) {
+//     product1.style.display = "none";    
+//     product2.style.display = "block";
+//     product3.style.display = "none";
+    
+  
+//   } else if ((product2.style.display !== "none") ) {
+//     product1.style.display = "none";
+//     product2.style.display = "none";
+//     product3.style.display = "block";
+//   } else {
+//     product1.style.display = "block";
+//     product2.style.display = "none";
+//     product3.style.display = "none";
+ 
+//   }
+// }
+
+
+// setInterval(toggleProducts, 8000); 
+
+// function changeproduct() {
+//   if ((product1.style.display !== "none")) {
+//     product1.style.display = "none";
+    
+//     product2.style.display = "block";
+ 
+//     product3.style.display = "none";
+  
+//   } else if ((product2.style.display !== "none") ) {
+//     product1.style.display = "none";
+    
+//     product2.style.display = "none";
+
+//     product3.style.display = "block";
+ 
+//   } else {
+//     product1.style.display = "block";
+  
+//     product2.style.display = "none";
+  
+//     product3.style.display = "none";
+ 
+//   }
+// }
+// function menubar() {
+//     let sidebar = document.getElementById('sidebar');
+//     let computedStyle = window.getComputedStyle(sidebar); // Get the computed style
+  
+//     if (computedStyle.display !== 'none') {
+//       sidebar.style.display = 'none';
+//     } else {
+//       sidebar.style.display = 'block';
+//     }
+// }
+
+  
+
+
